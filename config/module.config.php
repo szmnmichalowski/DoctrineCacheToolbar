5b9c6143b727ed5132865d632a41abd8ae41def7
Szymon Michalowski <szmnmichalowski@gmail.com>
2017-02-11 10:38:53 +0100

    Create project

---
+++ config/module.config.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ *
+ * Module.php
+ *
+ * @author:     Szymon Michałowski <szmnmichalowski@gmail.com>
+ * @data:       2017-02-08 19:36
+ */
+
+return [
+    'view_manager' => [
+        'template_map' => [
+        ],
+    ],
+    'zenddevelopertools' => [
+        'profiler' => [
+            'collectors' => [
+            ],
+        ],
+        'toolbar' => [
+            'entries' => [
+            ],
+        ],
+    ],
+];
---
+++ src/Module.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ *
+ * Module.php
+ *
+ * @author:     Szymon Michałowski <szmnmichalowski@gmail.com>
+ * @data:       2017-02-08 19:36
+ */
+
+namespace DoctrineCacheToolbar;
+
+use Zend\ModuleManager\Feature\ConfigProviderInterface;
+use Zend\ModuleManager\Feature\DependencyIndicatorInterface;
+
+/**
+ * Class Module
+ * @package DoctrineCacheToolbar
+ */
+class Module implements ConfigProviderInterface, DependencyIndicatorInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getConfig()
+    {
+        include __DIR__.'/../config/module.config.php';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getModuleDependencies()
+    {
+        return ['ZendDeveloperTools'];
+    }
+}
