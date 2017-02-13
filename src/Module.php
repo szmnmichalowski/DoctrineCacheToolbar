32b0057ceb4c42425fa5891a9d49051c38b7f803
Szymon Michalowski <szmnmichalowski@gmail.com>
2017-02-13 21:11:29 +0100

    Add MIT licence at the begining of file

--- src/Module.php
+++ src/Module.php
@@ -1,9 +1,21 @@
 <?php
 /**
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * This software consists of voluntary contributions made by many individuals
+ * and is licensed under the MIT license.
  *
  * Module.php
- *
- * @author:     Szymon Michałowski <szmnmichalowski@gmail.com>
  * @data:       2017-02-08 19:36
  */
 
@@ -11,14 +23,13 @@ namespace DoctrineCacheToolbar;
 
 use Zend\ModuleManager\Feature\ConfigProviderInterface;
 use Zend\ModuleManager\Feature\DependencyIndicatorInterface;
-use Zend\ModuleManager\ModuleManager;
-use Zend\Mvc\ModuleRouteListener;
 use Zend\Mvc\MvcEvent;
 use Doctrine\ORM\Cache\Logging\StatisticsCacheLogger;
 
 /**
  * Class Module
  * @package DoctrineCacheToolbar
+ * @author: Szymon Michałowski <szmnmichalowski@gmail.com>
  */
 class Module implements ConfigProviderInterface, DependencyIndicatorInterface
 {
